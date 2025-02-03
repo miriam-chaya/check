27001aab65ff606fb06e644db2165102ac5ed83f
miriam-chaya <ch63893@gmail.com>
2025-02-04 00:37:47 +0200

    secondi

--- Controllers/Logic.cs
+++ Controllers/Logic.cs
@@ -3,81 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace check.Controllers
 {
-    public class Logic : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Logic : ControllerBase
     {
         // GET: Logic
-        public ActionResult Index()
+       
+
+        [HttpGet]
+
+        public IActionResult FuncExercise(int num)
+
         {
-            return View();
+
+            
+            return Ok($"{num}* 5 = {num* 5}");
+
         }
 
-        // GET: Logic/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Logic/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Logic/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Logic/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Logic/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Logic/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Logic/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
+    
+
+        
+    
 }
